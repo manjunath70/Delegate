3e2361bffe4878a3e8fe6c8e2e3d684dbfe16796
manjunath <manjunath7068@gmail.com>
2022-02-18 19:54:37 +0530

    Initial Commit

---
+++ Delegate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Delegate
+//
+//  Created by manjunath on 18/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
